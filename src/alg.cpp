72e67413163850116ed70a613dc91c7758de2614
LesyaVasilek <125215125+LesyaVasilek@users.noreply.github.com>
2023-02-25 23:49:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,38 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+for (uint64_t i = 2; i < value; i++) {
+      if ((value % i) == 0)
+            return false;
+}
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+uint64_t upvalue = 0;
+int cur = 1;
+while (value != upvalue){
+    cur++;
+    if (checkPrime(cur))
+       upvalue++;
+  }
+  return cur;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+uint64_t search = value + 1;
+while (checkPrime(search) == false) {
+      search++;
+      }
+return search;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t sum = 0;
+for (uint64_t i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+        sum += i;
+       }
+   }
+return sum;
 }
