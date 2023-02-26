2e703ca919b5516ecfd5cfc4bb54a66d37b3ba5c
LesyaVasilek <125215125+LesyaVasilek@users.noreply.github.com>
2023-02-26 19:38:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,8 @@
+// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 bool checkPrime(uint64_t value) {
-    for (uint64_t i = 2; i < value; i++){
+    for (uint64_t i = 2; i < value; i++) {
         if ((value % i) == 0)
             return false;
     }
